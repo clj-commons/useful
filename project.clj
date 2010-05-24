a04a74d2f1047918b22bc931ab5e33d88b0ef160
Justin Balthrop <git@justinbalthrop.com>
2010-05-24 11:31:55 -0700

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-useful "0.1.0-SNAPSHOT"
+(defproject clojure-useful "0.1.1-SNAPSHOT"
   :description "useful clojure functions"
   :dependencies [[clojure "1.2.0-master-SNAPSHOT"]
                  [clojure-contrib "1.2.0-SNAPSHOT"]])
