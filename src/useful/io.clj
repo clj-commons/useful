8e8a0e665f0cd2f6058c96e8452a4fd47ec72cf9
Justin Balthrop <git@justinbalthrop.com>
2010-06-29 11:39:16 -0700

    add extract-resource functions to useful.io

---
+++ src/useful/io.clj
@@ -0,0 +1,20 @@
+(ns useful.io
+  (:use [clojure.java.io :only [copy]])
+  (:import [java.net URL URLConnection JarURLConnection]
+           [java.io File FileInputStream]))
+
+(defn resource-stream [name]
+  (if-let [url (.findResource (.getClassLoader clojure.lang.RT) name)]
+    (let [conn (.openConnection url)]
+      (if (instance? JarURLConnection conn)
+        (let [jar (cast JarURLConnection conn)]
+          (.getInputStream jar))
+        (FileInputStream. (File. (.getFile url)))))))
+
+(defn extract-resource [name dest-dir]
+  (if-let [s (resource-stream name)]
+    (let [dest (File. dest-dir name)]
+      (.mkdirs (.getParentFile dest))
+      (copy s dest)
+      dest)
+    (throw (Exception. (format "unable to find %s on classpath" name)))))
