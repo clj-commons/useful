6230af61240e7f7d3ad9274dcf5b5b5ccf2b8ae4
Justin Balthrop <git@justinbalthrop.com>
2010-08-19 08:42:42 -0700

    update to clojure 1.2.0

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject clojure-useful "0.2.6"
+(defproject clojure-useful "0.2.7"
   :description "useful clojure functions"
-  :dependencies [[clojure "1.2.0-master-SNAPSHOT"]])
+  :dependencies [[clojure "1.2.0"]])
