bd0fc693ed0ef4d10a19b5fe7080d32aedaa43e7
Alan Malloy <alan@malloys.org>
2011-06-24 14:16:32 -0700

    Fix that damn docstring AGAIN

--- src/useful/amalloy/debug.clj
+++ src/useful/amalloy/debug.clj
@@ -2,7 +2,7 @@
 
 (defmacro ?
   "A useful debugging tool when you can't figure out what's going on:
-  wrap a form with and-print, and the form will be printed alongside
+  wrap a form with ?, and the form will be printed alongside
   its result. The result will still be passed along."
   [val]
   `(let [x# ~val]
