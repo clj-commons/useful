7160bf7e5a7479ccbe6af16557af1d6bd8eddcaa
Alan Malloy <alan@malloys.org>
2011-06-24 15:50:48 -0700

    Put the rest of the stuff from filter into maps

--- src/useful/maps.clj
+++ src/useful/maps.clj
@@ -105,3 +105,36 @@
   [coll]
   (into {} (map-indexed (fn [idx val] [val idx])
                         (reverse coll)))) ; so earlier values clobber later ones
+
+(defn filter-keys-by-val
+  "Returns all keys in map for which (pred value) returns true."
+  [pred map]
+  (when map
+    (for [[key val] map :when (pred val)] key)))
+
+(defn remove-keys-by-val
+  "Returns all keys of map for which (pred value) returns false."
+  [pred map]
+  (filter-keys-by-val (complement pred) map))
+
+(defn filter-vals
+  "Returns a map that only contains values where (pred value) returns true."
+  [pred map]
+  (when map
+    (select-keys map (filter-keys-by-val pred map))))
+
+(defn remove-vals
+  "Returns a map that only contains values where (pred value) returns false."
+  [pred map]
+  (filter-vals (complement pred) map))
+
+(defn filter-keys
+  "Returns a map that only contains keys where (pred key) returns true."
+  [pred map]
+  (when map
+    (select-keys map (filter pred (keys map)))))
+
+(defn remove-keys
+  "Returns a map that only contains keys where (pred key) returns false."
+  [pred map]
+  (filter-keys (complement pred) map))
