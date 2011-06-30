a52dc385fd154d38af05c7a8a3823fe9769e308a
Alan Malloy <alan@malloys.org>
2011-06-30 12:52:26 -0700

    Add a test for position

--- test/useful/map_test.clj
+++ test/useful/map_test.clj
@@ -47,3 +47,7 @@
 (deftest test-index-by
   (is (= {true 3 false 4} (index-by odd? [1 3 4])))
   (is (= {1 2 3 4 5 6}    (index-by dec  [2 4 6]))))
+
+(deftest test-position
+  (is (= (position [1 3 5 3])
+         {1 0 3 1 5 2})))
