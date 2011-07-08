bdbb93c99057b73a34392f6a0124b39d413841ef
Alan Malloy <alan@malloys.org>
2011-07-08 14:49:35 -0700

    Fix default of keyed

--- src/useful/map.clj
+++ src/useful/map.clj
@@ -15,7 +15,7 @@
       "Create a map in which, for each symbol S in vars, (keyword S) is a
   key mapping to the value of S in the current scope. If passed an optional
   :strs or :syms first argument, use strings or symbols as the keys instead."
-    ([vars] `(keyed :keys ~keys))
+    ([vars] `(keyed :keys ~vars))
     ([key-type vars]
        (let [transform (comp (partial list `quote)
                              (transforms key-type))]
