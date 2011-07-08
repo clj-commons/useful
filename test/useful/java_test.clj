f0b907a2b29e01a851b808da74120052297dce33
Alan Malloy <alan@malloys.org>
2011-07-08 16:32:02 -0700

    Add test for invoke-private

--- test/useful/java_test.clj
+++ test/useful/java_test.clj
@@ -7,3 +7,12 @@
 
 (deftest test-construct
   (is (= "test" (construct String "test"))))
+
+(deftest test-invoke-private
+  (let [hash (doto (java.util.Hashtable.)
+               (.put 1 2)
+               (.put 3 4))]
+    (is (thrown? Throwable (.rehash hash)))
+    (is (= {1 2 3 4}
+           (doto hash (invoke-private "rehash"))))
+    (is (thrown? Throwable (.rehash hash)))))
