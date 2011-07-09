7fad1dfaa2b58f9327902b743f6a732753ae8452
Alan Malloy <alan@malloys.org>
2011-07-08 17:26:55 -0700

    Delete obsoleted functions

--- src/useful/io.clj
+++ src/useful/io.clj
@@ -4,30 +4,6 @@
            [java.io File FileInputStream PrintStream]
            [clojure.lang IDeref]))
 
-(defmacro multi-outstream [var]
-  (let [current-writer
-        `(let [w# ~var]
-           (if (instance? IDeref w#) (first @w#) w#))]
-    `(PrintStream.
-      (proxy [java.io.BufferedOutputStream] [nil]
-        (write
-          ([b#]           (.write ~current-writer b#))
-          ([b# off# len#] (.write ~current-writer b# off# len#)))
-        (flush [] (.flush ~current-writer))))))
-
-(defmacro with-outstream [bindings & forms]
-  `(do (doseq [[var# outs#] (partition 2 ~bindings)]
-         (swap! var# conj outs#))
-       (binding ~bindings ~@forms)
-       (doseq [[var# outs#] (partition 2 ~bindings)]
-         (doall (swap! var# (partial remove #(= outs# %)))))))
-
-(defn default-outstream-push [outs default]
-  (swap! outs conj default))
-
-(defn default-outstream-pop [outs default]
-  (doall (swap! outs (partial remove #(= default %)))))
-
 (defn resource-stream [name]
   (if-let [url (.findResource (.getClassLoader clojure.lang.RT) name)]
     (let [conn (.openConnection url)]
