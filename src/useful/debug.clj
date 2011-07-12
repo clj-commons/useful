3b1c55fb748dbaf9a25a608c33594fadea23df59
Alan Malloy <alan@malloys.org>
2011-07-12 15:06:34 -0700

    Add ?!

--- src/useful/debug.clj
+++ src/useful/debug.clj
@@ -1,10 +1,23 @@
-(ns useful.debug)
+(ns useful.debug
+  (:use clojure.stacktrace))
 
-(defmacro ?
-  "A useful debugging tool when you can't figure out what's going on:
+(letfn [(interrogate-form [list-head form]
+          `(let [display# (fn [val#]
+                            (~@list-head (prn-str '~form '~'is val#)))]
+             (try (doto ~form display#)
+                  (catch Throwable t#
+                    (display# {:thrown t#
+                               :trace (with-out-str
+                                        (print-cause-trace t#))})
+                    (throw t#)))))]
+
+  (defmacro ?
+    "A useful debugging tool when you can't figure out what's going on:
   wrap a form with ?, and the form will be printed alongside
   its result. The result will still be passed along."
-  [val]
-  `(let [x# ~val]
-     (prn '~val '~'is x#)
-     x#))
+    [val]
+    (interrogate-form `(print) val))
+
+  (defmacro ?!
+    [file val]
+    (interrogate-form `(spit ~file) val)))
