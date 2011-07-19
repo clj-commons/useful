6231ad85ab07000929c5081b581781b0eca6c6ab
Anthony Grimes <i@raynes.me>
2011-07-19 17:53:42 -0500

    Added cake-marginalia.

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject useful "0.5.3"
   :description "A collection of generally-useful Clojure utility functions"
   :dependencies [[clojure "1.2.0"]
-                 [org.clojure/tools.macro "0.1.1"]])
+                 [org.clojure/tools.macro "0.1.1"]]
+  :dev-dependencies [[org.clojars.flatland/cake-marginalia "0.6.1"]]
+  :tasks [cake-marginalia.tasks])
