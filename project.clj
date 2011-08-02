4c296e409264ad9e75290b084f1995f7d0658b01
Alan Malloy <alan@malloys.org>
2011-08-01 19:05:18 -0700

    Depend on new marginalia

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "A collection of generally-useful Clojure utility functions"
   :dependencies [[clojure "1.2.0"]
                  [org.clojure/tools.macro "0.1.1"]]
-  :dev-dependencies [[org.clojars.flatland/cake-marginalia "0.6.1"]]
+  :dev-dependencies [[org.clojars.flatland/cake-marginalia "0.6.2"]]
   :tasks [cake-marginalia.tasks])
