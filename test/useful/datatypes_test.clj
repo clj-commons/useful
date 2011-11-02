9e6ae80192c0d1602e0dfe13d68bc8287444363c
Alan Malloy <alan@malloys.org>
2011-11-02 16:02:32 -0700

    More thorough test for assoc/update record

--- test/useful/datatypes_test.clj
+++ test/useful/datatypes_test.clj
@@ -1,13 +1,13 @@
 (ns useful.datatypes-test
   (:use clojure.test useful.datatypes))
 
-(defrecord Test [a b])
+(defrecord Test [a b c])
 (record-accessors Test)
 
 (deftest test-record
-  (let [init (Test. 1 2)
-        second (Test. 1 5)]
-    (is (= init (make-record Test :b 2 :a 1)))
-    (is (= second (assoc-record init :b 5)))
-    (is (= second (update-record init (+ b 3))))
+  (let [init (Test. 1 2 3)
+        second (Test. 1 5 4)]
+    (is (= init (make-record Test :b 2 :a 1 :c 3)))
+    (is (= second (assoc-record init :b 5 :c 4)))
+    (is (= second (update-record init (+ b 3) (inc c))))
     (is (= (:a init) (a init)))))
