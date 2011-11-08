d4b4c0514077c9535ddc2f7c39d9be8d0cd0e1b9
Alan Malloy <alan@malloys.org>
2011-11-08 13:43:55 -0800

    Rename useful.alias to useful.ns, add var-name

--- src/useful/core.clj
+++ src/useful/core.clj
@@ -1,5 +1,5 @@
 (ns useful.core
-  (:use [useful.alias :only [alias-ns]]))
+  (:use [useful.ns :only [alias-ns]]))
 
 (doseq [module '(bean cli compress datatypes debug dispatch experimental experimental.unicode fn java macro map parallel seq state string test utils)]
   (alias-ns (symbol (str "useful." module))))
--- src/useful/experimental/unicode.clj
+++ src/useful/experimental/unicode.clj
@@ -2,7 +2,7 @@
   useful.experimental.unicode
   (:use [useful.utils :only [map-entry]]
         [useful.macro :only [macro-do]]
-        [useful.alias :only [defalias]]))
+        [useful.ns :only [defalias]]))
 
 (macro-do [dest src]
   `(defalias ~dest ~src)
