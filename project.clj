5e5f72b21d8f550a634a2eb50c45a98642e4b366
Alan Malloy <alan@malloys.org>
2012-02-23 17:28:48 -0800

    Depend on org.clojure/clojure, not just clojure.
    Lein 2.0 is removing this alias.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject useful "0.8.0-alpha1"
   :description "A collection of generally-useful Clojure utility functions"
-  :dependencies [[clojure "1.4.0-beta1"]
+  :dependencies [[org.clojure/clojure "1.4.0-beta1"]
                  [org.clojure/tools.macro "0.1.1"]]
-  :multi-deps {"1.3" [[clojure "1.3.0"]]
-               "1.2" [[clojure "1.2.1"]]
+  :multi-deps {"1.3" [[org.clojure/clojure "1.3.0"]]
+               "1.2" [[org.clojure/clojure "1.2.1"]]
                :all [[org.clojure/tools.macro "0.1.1"]]})
