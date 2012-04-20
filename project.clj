926896819f22e4d519f848fb112394fb3f9343fb
Justin Balthrop <git@justinbalthrop.com>
2012-04-19 18:57:35 -0700

    0.8.0-alpha3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject useful "0.8.0-alpha2"
+(defproject useful "0.8.0-alpha3"
   :description "A collection of generally-useful Clojure utility functions"
   :dependencies [[org.clojure/clojure "1.4.0-beta1"]
                  [org.clojure/tools.macro "0.1.1"]]
