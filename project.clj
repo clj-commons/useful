87d725ba56949a1f141b2c3b3cb71010ed8edaf8
Anthony Grimes <i@raynes.me>
2012-06-29 02:50:19 -0500

    Add :license and :url keys and inc to 0.8.3-alpha4.

--- project.clj
+++ project.clj
@@ -1,5 +1,9 @@
-(defproject useful "0.8.3-alpha3"
+(defproject useful "0.8.3-alpha4"
   :description "A collection of generally-useful Clojure utility functions"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo}
+  :url "https://github.com/flatland/useful"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.macro "0.1.1"]]
   :multi-deps {"1.3" [[org.clojure/clojure "1.3.0"]]
