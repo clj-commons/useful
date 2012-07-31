d38b6edea69c0db6a05dc645b5cd47ecbcc7a730
Alan Malloy <alan@malloys.org>
2012-07-31 12:33:23 -0700

    alpha9: add seque*

--- src/useful/seq.clj
+++ src/useful/seq.clj
@@ -310,3 +310,43 @@ ineligible for garbage collection."
              (do @worker nil)           ;just to propagate errors
              (cons (if (identical? x NIL) nil x)
                    (lazy-recur))))))))
+
+(defn seque*
+  "A version of clojure.core/seque that fixes a memory/thread-handle leak."
+  {:added "1.0"
+   :static true}
+  ([s] (seque 100 s))
+  ([n-or-q s]
+   (let [^BlockingQueue q (if (instance? BlockingQueue n-or-q)
+                             n-or-q
+                             (LinkedBlockingQueue. (int n-or-q)))
+         NIL (Object.) ;nil sentinel since LBQ doesn't support nils
+         agt (agent (sequence s)) ; never start with nil; that signifies we've already put eos
+         log-error (fn [q e]
+                     (if (.offer q q)
+                       (throw e)
+                       e))
+         fill (fn [s]
+                (when s
+                  (if (instance? Exception s) ; we failed to .offer an error earlier
+                    (log-error q s)
+                    (try
+                      (loop [[x & xs :as s] (seq s)]
+                        (if s
+                          (if (.offer q (if (nil? x) NIL x))
+                            (recur xs)
+                            s)
+                          (when-not (.offer q q) ; q itself is eos sentinel
+                            ()))) ; empty seq, not nil, so we know to put eos next time
+                      (catch Exception e
+                        (log-error q e))))))
+         drain (fn drain []
+                 (lazy-seq
+                  (let [x (.take q)]
+                    (if (identical? x q) ;q itself is eos sentinel
+                      (do @agt nil)  ;touch agent just to propagate errors
+                      (do
+                        (send-off agt fill)
+                        (cons (if (identical? x NIL) nil x) (drain)))))))]
+     (send-off agt fill)
+     (drain))))
