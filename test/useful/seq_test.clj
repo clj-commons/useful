529a1d9a82b67de5b1f360001a073e3b85ee3f4d
Justin Balthrop <git@justinbalthrop.com>
2012-10-08 17:08:33 -0700

    add update-first

--- test/useful/seq_test.clj
+++ test/useful/seq_test.clj
@@ -183,3 +183,10 @@
   (is (= [2 2 4 4 6 6 8 8 10 10]
          (map-nth inc 2 [1 2 3 4 5 6 7 8 9 10])))
   (is (= ["" "x" "" "x"] (map-nth #(str % "x") 1 2 ["" "" "" ""]))))
+
+(deftest test-update-first
+  (is (= [1 3 3 4 5]
+         (update-first [1 2 3 4 5] even? inc)))
+
+  (is (= [1 2 3 4 5 15]
+         (update-first [1 2 3 4 5] zero? (fnil + 0) 1 2 3 4 5))))
