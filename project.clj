49a6632d8895673b64cf01a1ef9dd0ded507be9c
Alan Malloy <alan@malloys.org>
2013-05-31 09:46:01 -0700

    Update testall alias to match clojure version

--- project.clj
+++ project.clj
@@ -7,6 +7,6 @@
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/tools.macro "0.1.1"]
                  [org.clojure/tools.reader "0.7.2"]]
-  :aliases {"testall" ["with-profile" "dev,default:dev,1.3,default:dev,1.5,default" "test"]}
+  :aliases {"testall" ["with-profile" "dev,default:dev,1.3,default:dev,1.4,default" "test"]}
   :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}})
