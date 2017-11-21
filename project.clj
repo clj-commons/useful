2bb1084dc80633c549b0735e9520501c44d7d7f9
Christopher Mark Gore <cgore@cgore.com>
2017-11-21 14:39:40 -0600

    Clojure 1.8

--- project.clj
+++ project.clj
@@ -4,14 +4,17 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :url "https://github.com/amalloy/useful"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.macro "0.1.1"]
-                 [org.clojure/tools.reader "0.7.2"]]
-  :aliases {"testall" ["with-profile" "dev,default:dev,1.3,default:dev,1.4,default:dev,1.5,default:dev,1.7,default" "test"]}
-  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha2"]]}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.macro "0.1.5"]
+                 [org.clojure/tools.reader "1.1.1"]]
+  :aliases {"testall" ["with-profile" "dev,default:dev,1.3,default:dev,1.4,default:dev,1.5,default:dev,1.6,default:dev,1.7,default" "test"]}
+  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]} }
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]
+                                  [org.clojure/tools.reader "0.7.2"]]}
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]
+                                  [org.clojure/tools.reader "0.7.2"]]}}
   :plugins [[codox "0.8.0"]]
   :codox {:src-dir-uri "http://github.com/flatland/useful/blob/develop/"
           :src-linenum-anchor-prefix "L"})
