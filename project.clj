88d94797bcbae0ec96405d67e1760fa9fd6be519
danielcompton <desk@danielcompton.net>
2018-12-06 20:44:33 +1300

    Increase test matrix

--- project.clj
+++ project.clj
@@ -4,14 +4,15 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :url "https://github.com/amalloy/useful"
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.macro "0.1.1"]
                  [org.clojure/tools.reader "0.7.2"]]
-  :aliases {"testall" ["with-profile" "dev,default:dev,1.3,default:dev,1.4,default:dev,1.5,default:dev,1.7,default" "test"]}
-  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha2"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]} }
+  :aliases {"testall" ["with-profile" "1.6:1.7:1.8:1.9:1.10" "test"]}
+  :profiles {:1.10 {:dependencies [[org.clojure/clojure "1.10.0-RC3"]]}
+             :1.9  {}
+             :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}}
   :plugins [[codox "0.8.0"]]
   :codox {:src-dir-uri "http://github.com/flatland/useful/blob/develop/"
           :src-linenum-anchor-prefix "L"})
