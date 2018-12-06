198e47afff459562b3ebbd90a45a4ddef37b90ce
danielcompton <desk@danielcompton.net>
2018-12-06 23:09:56 +1300

    Use java.util.Base64 instead of BASE64Encoder
    For backwards/forwards compatibility between implementations, adds a
    trailing newline to the string to match the behaviour of the sun.misc.BASE64Decoder.
    
    This effectively sets the minimum JVM to Java 8, at least to use this
    namespace.

--- project.clj
+++ project.clj
@@ -7,8 +7,9 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.macro "0.1.1"]
                  [org.clojure/tools.reader "0.7.2"]]
-  :aliases {"testall" ["with-profile" "1.6:1.7:1.8:1.9:1.10" "test"]}
-  :profiles {:1.10 {:dependencies [[org.clojure/clojure "1.10.0-RC3"]]}
+  :aliases {"testall" ["with-profile" "+1.6:+1.7:+1.8:+1.9:+1.10" "test"]}
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.10.0-alpha3"]]}
+             :1.10 {:dependencies [[org.clojure/clojure "1.10.0-RC3"]]}
              :1.9  {}
              :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
--- test/flatland/useful/compress_test.clj
+++ test/flatland/useful/compress_test.clj
@@ -1,5 +1,9 @@
 (ns flatland.useful.compress-test
-  (:use clojure.test flatland.useful.compress))
+  (:use clojure.test
+        flatland.useful.compress)
+  (:require [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]))
 
 
 (deftest round-trip
@@ -7,3 +11,16 @@
 g2 sd35g4szdf sdf4 as89faw76fwfwf210
 "]
     (is (= s (unsmash (smash s))))))
+
+(deftest smash-test
+  (is (= "eJwDAAAAAAE=\n" (smash-new "")))
+  (is (= "eJxLBAAAYgBi\n" (smash-new "a"))))
+
+(deftest unsmash-test
+  (is (= "" (unsmash-new "eJwDAAAAAAE=\n")))
+  (is (= "a" (unsmash-new "eJxLBAAAYgBi\n"))))
+
+(defspec round-trip-gen
+  100
+  (prop/for-all [s gen/string]
+    (= s (unsmash-new (smash s)))))
